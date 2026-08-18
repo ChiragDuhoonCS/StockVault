5b17e9ea8f9dded4948e39ade423da16c4c8699e
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-19 02:14:34 +0530

    Initial StockVault project setup

---
+++ strorage/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    printf("====================================\n");
+    printf("=          Storage Vault           =\n");
+    printf("   Inventory Management System     =\n");
+    printf("====================================\n");
+    
+    return 0;
+}
