d186bc748b12ba9bf892c17ccf82136e3760f0a3
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-19 17:12:48 +0530

    Product header file

--- src/product.h
+++ src/product.h
@@ -1,1 +1,15 @@
 // "What can the product module do?"
+
+#ifndef PRODUCT_H //"If PRODUCT_H has NOT been defined..."
+#define PRODUCT_H //"Define PRODUCT_H now."
+
+struct Product {
+    int product_id;
+    char product_name [50];
+    char category [50];
+    float price;
+    int quantity;
+    char supplier [100];
+};
+
+#endif
