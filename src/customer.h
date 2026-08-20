6f18e28a73cb09e65df9485972499c79736c28a7
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-20 14:23:15 +0530

    Add customer header file

---
+++ src/customer.h
@@ -0,0 +1,16 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+#define MAX_CUSTOMERS 100
+
+struct Customer {
+    int customer_id;
+    char customer_name[50];
+    char phone[15]; //! to add +
+    char email[100];
+    char address[100];
+};
+
+void addCustomer(struct Customer customers[], int *count);
+void displayCustomers(struct Customer customers[], int count);
+
+#endif
