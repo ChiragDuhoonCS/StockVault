b8e90427c8f3a20ece732270393bc9979ba610a3
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-20 14:59:33 +0530

    Fix bugs and improve exit feature

--- main.c
+++ main.c
@@ -8,65 +8,82 @@ int main() {
     printf("   Inventory Management System     =\n");
     printf("====================================\n");
 
-    struct Product inventory[MAX_PRODUCTS]; //@ for inventory in product
+    struct Product inventory[MAX_PRODUCTS];//@ for product.c
     struct Customer customers[MAX_CUSTOMERS];
-    
-    int count = 0;
-    int choice;
-    int choice1;
-    
-    printf("1. Product Management");
-    printf("2. Customer Management");
-    printf("3. Exit");
-    scanf("%d", &choice1);
-    getchar();
 
-    if (choice1 == 1){
-    while (1) { //@ 1 means always true here  loop will go forever
-        printf("\n----- Menu -----\n");
-        printf("1. Add Product\n");
-        printf("2. Display Products\n");
-        printf("3. Search Products\n");
-        printf("4. Update Products\n");
-        printf("5. Delete Products\n");
+    int productCount = 0;
+    int customerCount = 0;
+    int choice1;
+    int choice;
+
+    while (1) { //! means always true  loop will always go
+        printf("\n===== Main Menu =====\n");
+        printf("1. Product Management\n");
+        printf("2. Customer Management\n");
         printf("0. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
-        getchar();  // clear leftover newline before addProduct's fgets calls
-
-        if (choice == 1) { //! & this when we add  or delete
-            addProduct(inventory, &count);
-        } else if (choice == 2) { //! not & when we only read
-            displayProduct(inventory, count);
-        } else if (choice == 3) {
-            searchProduct(inventory, count);
-        }else if (choice == 4) {
-            updateProduct(inventory, count); 
-        }else if (choice == 5) {
-            deleteProduct(inventory,&count); 
-        }else if (choice == 0) {
-            printf("Exiting StockVault. Goodbye!\n");
-            break;
-        } else {
-            printf("Invalid choice. Try again.\n");
-        }
-    }
-    }
-
-    if (choice1 == 2) {
-        printf("\n----- Menu -----\n");
-        printf("1. Add Customer\n");
-        printf("2. Display Customers\n");
-        scanf("%d", &choice);
+        scanf("%d", &choice1);
         getchar();
 
-        if (choice == 1) { //! & this when we add  or delete
-            addCustomer(customers, &count);
-        } else if (choice == 2) { //! not & when we only read
-            displayCustomers(customers, count);
-        }else {
+        if (choice1 == 1) {
+            while (1) {
+                printf("\n----- Product Menu -----\n");
+                printf("1. Add Product\n");
+                printf("2. Display Products\n");
+                printf("3. Search Products\n");
+                printf("4. Update Products\n");
+                printf("5. Delete Products\n");
+                printf("0. Back to Main Menu\n");
+                printf("Enter your choice: ");
+                scanf("%d", &choice);
+                getchar();
+
+                if (choice == 1) {
+                    addProduct(inventory, &productCount);
+                } else if (choice == 2) {
+                    displayProduct(inventory, productCount);
+                } else if (choice == 3) {
+                    searchProduct(inventory, productCount);
+                } else if (choice == 4) {
+                    updateProduct(inventory, productCount);
+                } else if (choice == 5) {
+                    deleteProduct(inventory, &productCount);
+                } else if (choice == 0) {
+                    break;  // back to main menu
+                } else {
+                    printf("Invalid choice. Try again.\n");
+                }
+            }
+        }
+        else if (choice1 == 2) {
+            while (1) {
+                printf("\n----- Customer Menu -----\n");
+                printf("1. Add Customer\n");
+                printf("2. Display Customers\n");
+                printf("0. Back to Main Menu\n");
+                printf("Enter your choice: ");
+                scanf("%d", &choice);
+                getchar();
+
+                if (choice == 1) {
+                    addCustomer(customers, &customerCount);
+                } else if (choice == 2) {
+                    displayCustomers(customers, customerCount);
+                } else if (choice == 0) {
+                    break;  // back to main menu
+                } else {
+                    printf("Invalid choice. Try again.\n");
+                }
+            }
+        }
+        else if (choice1 == 0) {
+            printf("Exiting StockVault. Goodbye!\n");
+            break;  // exits the outer loop, program ends
+        }
+        else {
             printf("Invalid choice. Try again.\n");
         }
     }
+
     return 0;
 }
