ef4c2b6145e0c35b65fad1e32e9433c3a2b2fd75
Chirag Duhoon <chiragduhoon.cs@gmail.com>
2026-08-22 11:59:55 +0530

    Fix bug and add some comments

--- src/sale.h
+++ src/sale.h
@@ -2,17 +2,21 @@
 #define SALE_H
 #define MAX_SALES 100
 
+//! see here
 struct Sale {
-    int sale_id;
+    int sale_id; // sale id here
     int product_id;
-    int customerid;
+    int customer_id;
     int quantity_sold;
     float total_price;
 };
 
+//! see in sale count
 void makeSale(struct Product inventory[], int productCount,
               struct Customer customers[], int customerCount,
               struct Sale sales[], int *saleCount);
+
+//! read only so no *              
 void displaySales(struct Sale sales[], int saleCount);
 
 #endif
